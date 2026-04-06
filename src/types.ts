c067fc32b64cbf9ed6a08be34363d2d42f728f8c
hou5emd <andrandrey92@gmail.com>
2026-04-06 16:42:35 +0500

    feat: enhance type definitions for route and path builders with optional parameters and improved structure

--- src/types.ts
+++ src/types.ts
@@ -7,6 +7,29 @@ type UnionToIntersection<U> = (U extends unknown ? (k: U) => void : never) exten
   : never;
 
 type QueryKey = string | number;
+type SegmentKey = string | number;
+
+type ChildConfig<Config extends RouteConfig, Key extends keyof Config> = Extract<
+  Config[Key],
+  RouteConfig
+>;
+
+type ChildKeys<Config extends RouteConfig, Key extends keyof Config> = keyof ChildConfig<
+  Config,
+  Key
+>;
+
+type RequiredParameterName<Key> = Key extends `:${infer Name}` ? Name : never;
+
+type OptionalParameterName<Key> = Key extends `:${infer Name}?` ? Name : never;
+
+type ParameterName<Key> = Key extends `:${string}?`
+  ? OptionalParameterName<Key>
+  : RequiredParameterName<Key>;
+
+type IsOptionalParameter<Key> = Key extends `:${string}?` ? true : false;
+
+type StaticSegment<Key> = Extract<Key, SegmentKey>;
 
 type LastOfUnion<T extends QueryKey> =
   UnionToIntersection<T extends unknown ? (arg: T) => void : never> extends (
@@ -65,6 +88,140 @@ type GetByPath<
     ? T[Key]
     : never;
 
+type RelativeConfig<
+  Config extends RouteConfig,
+  BasePath extends string,
+  ConfigKey extends keyof Config,
+> = BasePath extends "/"
+  ? PathBuilder<Config, ConfigKey, "", "/">
+  : GetByPath<Config, BasePath> extends infer NextConfig
+    ? NextConfig extends RouteConfig
+      ? PathBuilder<NextConfig, keyof NextConfig, "", BasePath>
+      : never
+    : never;
+
+type RouteQueryNode<
+  Config extends RouteConfig,
+  Path extends string,
+> = "_queries" extends keyof Config
+  ? {
+      readonly _queries: <
+        Value extends string,
+        Queries extends { [Query in keyof Config["_queries"]]?: Value },
+      >(
+        queries: Queries,
+      ) => Builder<BuildPathWithQuery<Path, Queries>>;
+    }
+  : never;
+
+type RouteStaticChild<
+  Config extends RouteConfig,
+  Key extends keyof Config,
+  Path extends string,
+> = {
+  readonly [Property in StaticSegment<Key>]: RouteBuilder<
+    ChildConfig<Config, Key>,
+    ChildKeys<Config, Key>,
+    PathString<Path, StaticSegment<Key>>
+  >;
+};
+
+type RouteParameterChild<
+  Config extends RouteConfig,
+  Key extends keyof Config,
+  Path extends string,
+  Parameter extends string = ParameterName<Key>,
+> =
+  IsOptionalParameter<Key> extends true
+    ? {
+        readonly [Property in Parameter]: <Param extends string>(
+          parameter?: Param,
+        ) => RouteBuilder<
+          ChildConfig<Config, Key>,
+          ChildKeys<Config, Key>,
+          PathString<Path, Param>
+        >;
+      }
+    : {
+        readonly [Property in Parameter]: <Param extends string>(
+          parameter: Param,
+        ) => RouteBuilder<
+          ChildConfig<Config, Key>,
+          ChildKeys<Config, Key>,
+          PathString<Path, Param>
+        >;
+      };
+
+type RouteBuilderNode<
+  Config extends RouteConfig,
+  Key extends keyof Config,
+  Path extends string,
+> = Key extends "_queries"
+  ? RouteQueryNode<Config, Path>
+  : Key extends `:${string}`
+    ? RouteParameterChild<Config, Key, Path>
+    : Key extends SegmentKey
+      ? RouteStaticChild<Config, Key, Path>
+      : never;
+
+type PathStaticChild<
+  Config extends RouteConfig,
+  Key extends keyof Config,
+  Path extends string,
+  RelativeToPath extends string,
+  NextPath extends string = PathString<Path, StaticSegment<Key>, RelativeToPath>,
+> = {
+  readonly [Property in StaticSegment<Key>]: PathBuilder<
+    ChildConfig<Config, Key>,
+    ChildKeys<Config, Key>,
+    NextPath,
+    RelativeToPath
+  >;
+};
+
+type PathParameterChild<
+  Config extends RouteConfig,
+  Key extends keyof Config,
+  Path extends string,
+  RelativeToPath extends string,
+  Parameter extends string = ParameterName<Key>,
+  NextPath extends string = PathString<Path, Key & SegmentKey, RelativeToPath>,
+> = {
+  readonly [Property in Parameter]: PathBuilder<
+    ChildConfig<Config, Key>,
+    ChildKeys<Config, Key>,
+    NextPath,
+    RelativeToPath
+  >;
+};
+
+type PathBuilderNode<
+  Config extends RouteConfig,
+  Key extends keyof Config,
+  Path extends string,
+  RelativeToPath extends string,
+> = Key extends "_queries"
+  ? never
+  : Key extends `:${string}`
+    ? PathParameterChild<Config, Key, Path, RelativeToPath>
+    : Key extends SegmentKey
+      ? PathStaticChild<Config, Key, Path, RelativeToPath>
+      : never;
+
+type RelativeToNode<
+  Config extends RouteConfig,
+  ConfigKey extends keyof Config,
+  Path extends string,
+> = Path extends "/"
+  ? {
+      relativeTo: <PB extends Builder>(
+        base: PB,
+      ) => PB extends Builder<infer BasePath extends string>
+        ? RelativeConfig<Config, BasePath, ConfigKey>
+        : never;
+    }
+  : {};
+
 export type RouteConfig<
   DeepLength extends number = 50,
   DeepLengthArr extends number[] = [0],
@@ -93,37 +250,12 @@ export type PathString<
         : `${Current}/${NextPath}`;
 
 export type RouteBuilder<
-  T extends RouteConfig,
-  K extends keyof T = keyof T,
+  Config extends RouteConfig,
+  ConfigKey extends keyof Config = keyof Config,
   Path extends string = "",
 > = Builder<Path> &
   UnionToIntersection<
-    K extends "_queries"
-      ? {
-          readonly [M in K]: <
-            V extends string,
-            Queries extends { [Q in keyof T[M]]?: V },
-          >(
-            queries: Queries,
-          ) => Builder<BuildPathWithQuery<Path, Queries>>;
-        }
-      : K extends `:${infer N}?`
-        ? {
-            readonly [M in N]: <Param extends string>(
-              parameter?: Param,
-            ) => RouteBuilder<T[K], keyof T[K], PathString<Path, Param>>;
-          }
-        : K extends `:${infer N}`
-          ? {
-              readonly [M in N]: <Param extends string>(
-                parameter: Param,
-              ) => RouteBuilder<T[K], keyof T[K], PathString<Path, Param>>;
-            }
-          : K extends string | number
-            ? {
-                readonly [M in K]: RouteBuilder<T[K], keyof T[K], PathString<Path, K>>;
-              }
-            : never
+    ConfigKey extends keyof Config ? RouteBuilderNode<Config, ConfigKey, Path> : never
   >;
 
 export type PathBuilder<
@@ -133,46 +265,8 @@ export type PathBuilder<
   RelativeToPath extends string = "",
 > = Builder<Path> &
   UnionToIntersection<
-    ConfigKey extends "_queries"
-      ? never
-      : ConfigKey extends `:${infer N}?`
-        ? {
-            readonly [M in N]: PathBuilder<
-              Config[ConfigKey],
-              keyof Config[ConfigKey],
-              PathString<Path, ConfigKey, RelativeToPath>
-            >;
-          }
-        : ConfigKey extends `:${infer N}`
-          ? {
-              readonly [M in N]: PathBuilder<
-                Config[ConfigKey],
-                keyof Config[ConfigKey],
-                PathString<Path, ConfigKey, RelativeToPath>
-              >;
-            }
-          : ConfigKey extends string | number
-            ? {
-                readonly [M in ConfigKey]: PathBuilder<
-                  Config[ConfigKey],
-                  keyof Config[ConfigKey],
-                  PathString<Path, ConfigKey, RelativeToPath>
-                >;
-              }
-            : never
+    ConfigKey extends keyof Config
+      ? PathBuilderNode<Config, ConfigKey, Path, RelativeToPath>
+      : never
   > &
-  (Path extends "/"
-    ? {
-        relativeTo: <PB extends Builder>(
-          base: PB,
-        ) => PB extends Builder<infer BasePath>
-          ? BasePath extends "/"
-            ? PathBuilder<Config, ConfigKey, "", "/">
-            : GetByPath<Config, BasePath> extends infer NewConfig
-              ? NewConfig extends RouteConfig
-                ? PathBuilder<NewConfig, keyof NewConfig, "", BasePath>
-                : never
-              : never
-          : never;
-      }
-    : {});
+  RelativeToNode<Config, ConfigKey, Path>;
